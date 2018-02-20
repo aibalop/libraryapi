d5ac8dee220a02379061e29c5b5d90066fec604f
Alan Ibarra <aledibarralo@ittepic.edu.mx>
2018-02-20 16:27:28 -0700

    Feat: adding version´s number

--- server.js
+++ server.js
@@ -6,9 +6,15 @@ var bodyParser = require('body-parser');
 var api = require('./routes/routes');
 
 var app = express();
+var version = "v1";
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/api',api);
+app.use(`/api/${version}/}`, api);
 
 module.exports = app;
